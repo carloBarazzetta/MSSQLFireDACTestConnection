f57f0343819184d0be4033b5f351db11d70a9867
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2024-10-15 21:27:37 +0200

    test project

---
+++ Source/Forms.Main.dfm
@@ -0,0 +1,146 @@
+object MainForm: TMainForm
+  Left = 0
+  Top = 0
+  ClientHeight = 415
+  ClientWidth = 854
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnCreate = FormCreate
+  DesignSize = (
+    854
+    415)
+  TextHeight = 13
+  object ParamsLabel: TLabel
+    Left = 8
+    Top = 51
+    Width = 99
+    Height = 13
+    Caption = 'Connection Params'
+  end
+  object ReportLabel: TLabel
+    Left = 262
+    Top = 51
+    Width = 98
+    Height = 13
+    Caption = 'Connection Report'
+  end
+  object Label1: TLabel
+    Left = 8
+    Top = 271
+    Width = 117
+    Height = 13
+    Caption = 'Available ODBC Drivers'
+  end
+  object ConnectionStringEdit: TLabeledEdit
+    Left = 8
+    Top = 24
+    Width = 838
+    Height = 21
+    Anchors = [akLeft, akTop, akRight]
+    EditLabel.Width = 91
+    EditLabel.Height = 13
+    EditLabel.Caption = 'ConnectionString'
+    TabOrder = 0
+    Text = ''
+    OnChange = ConnectionStringEditChange
+  end
+  object ConnectButton: TButton
+    Left = 135
+    Top = 362
+    Width = 121
+    Height = 25
+    Anchors = [akLeft, akBottom]
+    Caption = 'Connect...'
+    TabOrder = 7
+    OnClick = ConnectButtonClick
+  end
+  object ReportMemo: TMemo
+    Left = 262
+    Top = 67
+    Width = 584
+    Height = 320
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    ReadOnly = True
+    ScrollBars = ssVertical
+    TabOrder = 2
+  end
+  object LoginPromptCheckBox: TCheckBox
+    Left = 8
+    Top = 364
+    Width = 97
+    Height = 17
+    Anchors = [akLeft, akBottom]
+    Caption = 'Login Prompt'
+    TabOrder = 6
+    OnClick = LoginPromptCheckBoxClick
+  end
+  object UserNameEdit: TLabeledEdit
+    Left = 8
+    Top = 335
+    Width = 121
+    Height = 21
+    Anchors = [akLeft, akBottom]
+    EditLabel.Width = 55
+    EditLabel.Height = 13
+    EditLabel.Caption = 'User Name'
+    TabOrder = 4
+    Text = 'sa'
+    OnChange = UserNameEditChange
+  end
+  object PasswordEdit: TLabeledEdit
+    Left = 135
+    Top = 335
+    Width = 121
+    Height = 21
+    Anchors = [akLeft, akBottom]
+    EditLabel.Width = 49
+    EditLabel.Height = 13
+    EditLabel.Caption = 'Password'
+    TabOrder = 5
+    Text = '12345'
+    OnChange = PasswordEditChange
+  end
+  object ParamsMemo: TMemo
+    Left = 8
+    Top = 67
+    Width = 248
+    Height = 198
+    Anchors = [akLeft, akTop, akBottom]
+    Lines.Strings = (
+      'ParamsMemo')
+    TabOrder = 1
+    OnChange = ParamsMemoChange
+  end
+  object StatusBar: TStatusBar
+    Left = 0
+    Top = 396
+    Width = 854
+    Height = 19
+    Panels = <>
+    SimplePanel = True
+  end
+  object DriverComboBox: TComboBox
+    Left = 8
+    Top = 288
+    Width = 248
+    Height = 21
+    Anchors = [akLeft, akBottom]
+    DropDownCount = 10
+    TabOrder = 3
+    Items.Strings = (
+      '')
+  end
+  object FDConnection: TFDConnection
+    LoginPrompt = False
+    Left = 312
+    Top = 208
+  end
+  object FDPhysMSSQLDriverLink: TFDPhysMSSQLDriverLink
+    Left = 424
+    Top = 216
+  end
+end
---
+++ Source/Forms.Main.pas
@@ -0,0 +1,162 @@
+unit Forms.Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, Vcl.StdCtrls, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client, FireDAC.Phys.ODBCBase, FireDAC.Phys.MSSQL,
+  FireDAC.Phys.MSSQLDef, Vcl.Mask, Vcl.ExtCtrls, Vcl.ComCtrls,
+  FireDAC.Phys.MSAccDef, FireDAC.Phys.MSAcc;
+
+type
+  TMainForm = class(TForm)
+    FDConnection: TFDConnection;
+
+    ConnectionStringEdit: TLabeledEdit;
+    ConnectButton: TButton;
+    ReportMemo: TMemo;
+    LoginPromptCheckBox: TCheckBox;
+    UserNameEdit: TLabeledEdit;
+    PasswordEdit: TLabeledEdit;
+    ParamsMemo: TMemo;
+    ParamsLabel: TLabel;
+    ReportLabel: TLabel;
+    StatusBar: TStatusBar;
+    DriverComboBox: TComboBox;
+    FDPhysMSSQLDriverLink: TFDPhysMSSQLDriverLink;
+    Label1: TLabel;
+    procedure ConnectButtonClick(Sender: TObject);
+    procedure LoginPromptCheckBoxClick(Sender: TObject);
+    procedure ConnectionStringEditChange(Sender: TObject);
+    procedure UserNameEditChange(Sender: TObject);
+    procedure PasswordEditChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure ParamsMemoChange(Sender: TObject);
+  private
+    procedure UpdateParamsList;
+    procedure InitValuesFromIniFile;
+  public
+    { Public declarations }
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+uses
+  System.IniFiles
+  ;
+
+{$R *.dfm}
+
+procedure TMainForm.ConnectButtonClick(Sender: TObject);
+var
+  LCustomDriver: string;
+begin
+  FDConnection.Connected := False;
+  StatusBar.SimpleText := 'Not Connected';
+  if not FDConnection.LoginPrompt then
+  begin
+    FDConnection.Params.Values['User_Name'] := 'sa';
+    FDConnection.Params.Values['Password'] := '12345';
+  end;
+
+  LCustomDriver := DriverComboBox.Text;
+
+  if LCustomDriver <> '' then
+    FDPhysMSSQLDriverLink.ODBCDriver := LCustomDriver;
+  try
+    if Assigned(FDConnection.ConnectionIntf) then
+      FDConnection.ConnectionIntf.ForceDisconnect;
+    FDConnection.Connected := True;
+    if FDConnection.Connected then
+      StatusBar.SimpleText := 'Connected'
+    else
+      StatusBar.SimpleText := 'Connection Error';
+    FDConnection.GetInfoReport(ReportMemo.Lines);
+  except
+    StatusBar.SimpleText := 'Connection Error';
+    FDConnection.GetInfoReport(ReportMemo.Lines);
+    raise;
+  end;
+end;
+
+procedure TMainForm.ConnectionStringEditChange(Sender: TObject);
+begin
+  if ConnectionStringEdit.Text <> '' then
+  begin
+    FDConnection.ConnectionString := ConnectionStringEdit.Text;
+    UpdateParamsList;
+  end;
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  Caption := Application.Title;
+  FDPhysMSSQLDriverLink.GetDrivers(DriverComboBox.Items);
+  InitValuesFromIniFile;
+end;
+
+procedure TMainForm.InitValuesFromIniFile;
+var
+  LIniFile: TIniFile;
+  LDriver: string;
+begin
+  LIniFile := TIniFile.create(ChangeFileExt(Application.ExeName, '.ini'));
+  try
+    ConnectionStringEdit.Text := LIniFile.ReadString('Connection', 'ConnectionString', '');
+    UpdateParamsList;
+    LIniFile.ReadSectionValues('ConnectionParams', ParamsMemo.Lines);
+    UserNameEdit.Text := LIniFile.ReadString('LoginParams', 'User_Name', '');
+    PasswordEdit.Text := LIniFile.ReadString('LoginParams', 'Password', '');
+    LoginPromptCheckBox.Checked := LIniFile.ReadBool('LoginParams', 'LoginPrompt', False);
+    LDriver := LIniFile.ReadString('DriverLink', 'ODBCDriver', '');
+    if LDriver <> '' then
+      DriverComboBox.ItemIndex := DriverComboBox.Items.IndexOf(LDriver);
+  finally
+    LIniFile.Free;
+  end;
+end;
+
+procedure TMainForm.LoginPromptCheckBoxClick(Sender: TObject);
+begin
+  FDConnection.LoginPrompt := LoginPromptCheckBox.Checked;
+  UpdateParamsList;
+end;
+
+procedure TMainForm.ParamsMemoChange(Sender: TObject);
+begin
+  if Trim(ParamsMemo.Text) <> '' then
+  begin
+    FDConnection.Params.Text := Trim(ParamsMemo.Text);
+    UpdateParamsList;
+  end;
+end;
+
+procedure TMainForm.PasswordEditChange(Sender: TObject);
+begin
+  FDConnection.Params.Values['Password'] := PasswordEdit.Text;
+  UpdateParamsList;
+end;
+
+procedure TMainForm.UpdateParamsList;
+begin
+  ParamsMemo.Lines.Text := FDConnection.Params.Text;
+  PasswordEdit.Text := FDConnection.Params.Values['Password'];
+  UserNameEdit.Text := FDConnection.Params.Values['User_Name'];
+  ConnectionStringEdit.Text := FDConnection.ConnectionString;
+end;
+
+procedure TMainForm.UserNameEditChange(Sender: TObject);
+begin
+  FDConnection.Params.Values['User_Name'] := UserNameEdit.Text;
+  UpdateParamsList;
+end;
+
+end.
